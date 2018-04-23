71fb5f36137f5087d18a275c22085b5972797dba
Mr RA Venter <18439713@sun.ac.za>
2018-04-23 15:12:03 +0200

    Initial Commit

---
+++ quiz.php
@@ -0,0 +1,22 @@
+<html lang="em">
+<head>
+<link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+<title>18439713 Quiz Results</title>
+<link rel="stylesheer" type"text/css" href="jscript.css">
+<meta charset="utf-8">
+<meta name="viewpoint" contents="width=device-width, initial-scale=1">
+</head>
+
+<body>
+	<?php
+		echo "<h1> OWL Test Answered </h1>";
+
+		$Q1_answer = $_POST[Q1];
+
+		echo $Q1_answer."<br>";
+
+	?>
+
+
+</body>
+</html>
